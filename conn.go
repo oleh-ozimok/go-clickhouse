ec38130bccb5b9a502b1ac00deaaff63c87626bf
Andrii Filenko <andrey.filenko@corp.kismia.com>
2021-02-17 18:33:49 +0200

    refactor iterator

--- conn.go
+++ conn.go
@@ -2,6 +2,7 @@ package clickhouse
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -15,11 +16,16 @@ type Conn struct {
 }
 
 func (c *Conn) Ping() (err error) {
-	var res string
-	res, err = c.transport.Exec(c, Query{Stmt: ""}, true)
+	body, err := c.transport.Exec(c, Query{Stmt: ""}, true)
+	if err != nil {
+		return err
+	}
+	defer body.Close()
+	bodyBytes, err := ioutil.ReadAll(body)
+	response := string(bodyBytes)
 	if err == nil {
-		if !strings.Contains(res, successTestResponse) {
-			err = fmt.Errorf("Clickhouse host response was '%s', expected '%s'.", res, successTestResponse)
+		if !strings.Contains(response, successTestResponse) {
+			err = fmt.Errorf("Clickhouse host response was '%s', expected '%s'.", response, successTestResponse)
 		}
 	}
 
